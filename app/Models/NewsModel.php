68e0635566a52bff7d21bdf7ee2d926af72b7f7c
wood08 <wood08@hotmail.com>
2019-07-02 13:32:34 +0900

    ci4 tutorial

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -5,6 +5,7 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
 	protected $table = 'news';
+
 	protected $allowedFields = ['title', 'slug', 'body'];
 
 	public function getNews($slug = false)
